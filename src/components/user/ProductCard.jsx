4bcc03dd4ce91ef888f4a536d5e4f46500460b2c
[nirmal] <nirmallamrin77@gmail.com>
2024-06-18 21:40:47 +0530

    initial commit

--- src/components/user/ProductCard.jsx
+++ src/components/user/ProductCard.jsx
@@ -16,32 +16,36 @@ import {
 
 
 const ProductCard = ({product}) => {  
-  return (        
-            <Card maxW="sm" className="w-64">
-              
-              <CardBody>
-                <Image
-                 
-                  maxW={{ base: "100%", sm: "200px" }}
-                  src={product.image.url}
-                  alt={product.title}
-                  borderRadius="lg"
-                  className="w-full h-48 object-cover"
-                />
-                <Stack mt="6" spacing="3">
-                  <Heading size="md">{product.title}</Heading>
-                  
+  return (
+    <Card maxW="sm" className="w-64">
+      <CardBody>
+        <Image
+          maxW={{ base: "100%", sm: "200px" }}
+          src={product.image.url}
+          alt={product.title}
+          borderRadius="lg"
+          className="w-full h-48 object-cover"
+        />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">{product.title}</Heading>
 
-                  <Text color="blue.600" fontSize="">
-                   INR {product.price}
-                  </Text>
-                </Stack>
-              </CardBody>
-              <Divider />
-              
-              
-            </Card>
-         
+          <Text color="blue.600" fontSize="">
+            INR {product.price}
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <ButtonGroup spacing="2">
+          <Button variant="solid" colorScheme="blue">
+            Buy now
+          </Button>
+          <Button variant="ghost" colorScheme="blue">
+            Add to cart
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
   );
 };
 
