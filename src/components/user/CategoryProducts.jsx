3e37f983104642100c5d8d97d4d6e2b54c7eb1a1
[nirmal] <nirmallamrin77@gmail.com>
2024-06-21 13:50:31 +0530

    initial commit

--- src/components/user/CategoryProducts.jsx
+++ src/components/user/CategoryProducts.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import ProductCard from './ProductCard';
-import { useDispatch, useSelector } from 'react-redux';
+
 
 const CategoryProducts = () => {
     const {categoryName } = useParams();
