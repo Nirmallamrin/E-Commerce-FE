8210a61b844618d4822b4f5d7e31157692491b8e
[nirmal] <nirmallamrin77@gmail.com>
2024-06-24 21:41:20 +0530

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-import { ShoppingCartIcon, UserIcon } from "@heroicons/react/outline";
 import {
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
   IconButton,
-  Button,
 } from "@chakra-ui/react";
-import {
-  HamburgerIcon,
-  AddIcon,
-  ExternalLinkIcon,
-  RepeatIcon,
-  EditIcon,
-  ChevronDownIcon,
-} from "@chakra-ui/icons";
-import { SearchIcon } from "@heroicons/react/solid";
+import {HamburgerIcon} from "@chakra-ui/icons";
+import { SearchIcon, ShoppingCartIcon } from "@heroicons/react/solid";
+import { MdAccountCircle } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { toast } from "react-toastify";
@@ -32,47 +24,54 @@ const Navbar = () => {
     navigate("/");
   };
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+  const handleSearchChange = () => {
+
+  }
+
+  const handleSearchSubmit = () => {
+
+  }
+
+  // const toggleDropdown = () => {
+  //   setIsOpen(!isOpen);
+  // };
+
   return (
-    <nav className="bg-cyan-100  p-6">
+    <nav className="bg-cyan-100  p-4 md:p-6 shadow-md">
       <div className="max-w-7xl mx-auto flex items-center justify-between flex-wrap">
         <div className="flex items-center space-x-4">
           <Link to="/">
-            <span className="text-slate-950 text-lg font-bold">Shopy</span>
+            <span className="text-slate-950 text-3xl font-bold">Shopy</span>
           </Link>
-          <label className="relative block w-80">
+          <form onSubmit={handleSearchSubmit} className="relative block w-full md:w-96 max-w-xs">
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-              <SearchIcon
-                className="h-5 w-5 text-slate-400"
-                aria-hidden="true"
-              />
+              <SearchIcon className="h-5 w-5 text-slate-400" aria-hidden="true" />
             </span>
             <input
               className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               placeholder="Search..."
               type="text"
-              name="search"
+              
+              onChange={handleSearchChange}
             />
-          </label>
+          </form>
         </div>
-        <div className="flex items-center space-x-4 mt-4 md:mt-0">
-          <ul className="flex space-x-2">
+
+        <div className=" hidden md:flex items-center space-x-4 mt-4 md:mt-0">
+
+          <ul className="flex space-x-4">
             <Link to="/users/signin">
-              <li className="text-slate-950 cursor-pointer hover:underline">
-                Login
+              <li className="  text-slate-950 cursor-pointer hover:underline flex items-center">
+              <MdAccountCircle className="h-5 w-5 mr-1"/> Login
               </li>
             </Link>
-          </ul>
-          <ul className="flex space-x-2">
+
             <Link to="cart">
-              <li className="text-slate-950  cursor-pointer hover:underline">
-                Cart
+            <li className="text-slate-950 cursor-pointer hover:underline flex items-center">
+                <ShoppingCartIcon className="h-5 w-5 mr-1" /> Cart
               </li>
             </Link>
-          </ul>
-          <ul className="flex space-x-2">
+
             <Link to="orders">
               <li className="text-slate-950  cursor-pointer hover:underline">
                 MyOrders
